e382e47630962645c5e2a166ec5797587ec3195e
Vyacheslav Krachkov <v.krachkov@enestech.net>
2022-11-03 23:48:28 +0200

    chg: remove websockets-proxy

--- fastapi-stomp/main.py
+++ fastapi-stomp/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 
 import uvicorn
 import websockets
@@ -13,7 +14,7 @@ STOMP_URL = "ws://localhost:15674/ws"
 
 async def forward(ws: WebSocket, stomp_ws: websockets.WebSocketClientProtocol):
     while True:
-        data = await ws.receive_bytes()
+        data = await ws.receive_text()
         print("websocket A received:", data)
         await stomp_ws.send(data)
 
@@ -27,16 +28,16 @@ async def reverse(ws: WebSocket, stomp_ws: websockets.WebSocketClientProtocol):
 
 @app.websocket("/stomp")
 async def websocket(ws: WebSocket):
-    await ws.accept()
-    print(ws.headers)
+    await ws.accept(subprotocol="v11.stomp")
     stomp_ws: websockets.WebSocketClientProtocol
     async with websockets.connect(
         STOMP_URL, subprotocols=[Subprotocol("v10.stomp"), Subprotocol("v11.stomp")],
     ) as stomp_ws:
-        fwd_task = asyncio.create_task(forward(ws, stomp_ws))
         rev_task = asyncio.create_task(reverse(ws, stomp_ws))
+        fwd_task = asyncio.create_task(forward(ws, stomp_ws))
         await asyncio.gather(fwd_task, rev_task)
 
 
 if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s: %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
     uvicorn.run(app)
